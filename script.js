8143d8c750da08f5c0fcdffdda08cfad043ca35a
NadyaGolub <nadyagolub@ukr.net>
2025-01-30 15:28:12 +0200

    test

--- script.js
+++
@@ -1,7 +0,0 @@
-const elements = document.querySelectorAll('.name');
-
-elements.forEach(element => {
-    element.addEventListener('input', () => {
-        element.style.width = element.scrollWidth + 'px';
-    });
-});
